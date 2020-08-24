269cb1797c3febd77cde74459a7f8db4867dab88
AdrienF1019 <54679500+AdrienF1019@users.noreply.github.com>
2020-08-24 17:45:05 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,15 @@
+/**
+ * Author: Adrien Fortner
+ * Date: 2020/08/24
+ *
+ * Hack 1 basic print code
+ */
+ #include<stdlib.h>
+ #include<stdio.h>
+
+ int main(int argc, char **argv){
+
+     printf("Adrien Fortner\n");
+
+     return 0;
+ }
